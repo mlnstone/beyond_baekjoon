cb9b23c2ef3298693e4a51fd6ac9925cffcdcfb8
dnswjs1776 <dnswjs1776@naver.com>
2025-02-14 14:24:12 +0900

    test

--- src/step7/Ba_2563.java
+++ src/step7/Ba_2563.java
@@ -9,12 +9,12 @@ public class Ba_2563 {
         int n = sc.nextInt();
         int[][] arr = new int[n][2];
         int[][] area = new int[100][100];
-        for(int i = 0 ; i < n; i++){
+        for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
 
-            for(int j = 0; j < 10; j++){
-                for(int k = 0 ; k < 10; k++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
 
                     area[x + j][y + k] = 1;
 
@@ -26,14 +26,12 @@ public class Ba_2563 {
         int count = 0;
         for (int[] ints : area) {
             for (int anInt : ints) {
-                if(anInt == 1)
+                if (anInt == 1)
                     count++;
             }
 
         }
         System.out.println(count);
-
-
 
 
     }
